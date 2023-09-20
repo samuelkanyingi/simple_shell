57f5fa510ef2808772896f2e1954ad036ffcdc71
samuelkanyingi <samuelkanyingi2016@gmail.com>
2023-09-20 00:30:40 -0700

    simple_shell

--- atoi.c
+++ atoi.c
@@ -1,11 +1,15 @@
 #include "listme.h"
-
+/**
+ * custom_atoi -convert string to integer
+ * @string:string to convert
+ * Return:integer
+ */
 int custom_atoi(const char *string)
 {
 	int num_sign = 1;
 	int res = 0;
 
-	for (;*string == ' ';string++)
+	for (; *string == ' '; string++)
 	{
 		string++;
 	}
@@ -17,10 +21,9 @@ int custom_atoi(const char *string)
 	{
 		string++;
 	}
-	for (;*string >= '0' && *string <= '9';string++)
+	for (; *string >= '0' && *string <= '9'; string++)
 	{
 		res = res * 10 + (*string - '0');
-		
 	}
 	return (num_sign * res);
 }
